976d2b0202496091006d975330291039f2a8b2cb
LilyLaw <1444987087@qq.com>
2019-02-22 15:59:25 +0800

    交互

--- 移动端贺卡/script.js
+++ 移动端贺卡/script.js
@@ -1,4 +1,50 @@
-var music = document.getElementById("music");
-music.onclick = function(){
-	
+window.onload = function(){
+	var music = document.getElementById("music");
+	var audio = document.getElementsByTagName("audio")[0];
+	var page1 = document.getElementById("page1");
+	var page2 = document.getElementById("page2");
+	var page3 = document.getElementById("page3");
+
+
+	audio.addEventListener("ended",function(){
+		music.classList.remove("play");
+		// music.animationPlayState = "paused";
+		// music.webkitAnimationPlayState = "paused";
+	},false)
+
+	// music.onclick = function(){
+	// 	if(audio.paused){
+	// 		audio.play();
+	// 		music.classList.add("play");
+	// 		// this.style.animationPlayState = "running";
+	// 		// this.style.webkitAnimationPlayState = "running";
+	// 	}else{
+	// 		audio.pause();
+	// 		music.classList.remove("play");
+	// 		// this.style.animationPlayState = "paused";
+	// 		// this.style.webkitAnimationPlayState = "paused";
+	// 	}
+	// }
+
+	music.addEventListener("touchstart",function(){
+		if(audio.paused){
+			audio.play();
+			music.classList.add("play");
+		}else{
+			audio.pause();
+			music.classList.remove("play");
+		}
+	},false);
+
+	page1.ontouchstart = function(){
+		this.style.display = "none";
+		page2.style.display = "block";
+		page3.style.display = "block";
+		page3.style.top = "100%";
+
+		setTimeout(()=>{
+			page2.classList.add("fadeOut");
+			page3.classList.add("fadeIn");
+		},5500)
+	}
 }
